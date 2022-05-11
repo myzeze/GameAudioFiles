7a3b6b01f361077f397bd64a442eb4d8d3df0d07
twd99 <100174053+twd99@users.noreply.github.com>
2022-05-12 00:36:30 +0100

    arguing men
    couldnt get script to work to trigger an event for the arguing men so FMOD Emitter was used and a loop of a multi instrument in FMOD

---
+++ AudioGame/Assets/Unity Technologies/Adventure - Sample Game/Scripts/MonoBehaviours/Player/SalesMan.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalesMan : MonoBehaviour {
+
+
+
+
+
+
+
+    void SalesManSound()
+    
+      {
+        FMODUnity.RuntimeManager.PlayOneShot ("event:/Fruit Vendors/Shop Keeper");
+
+      
+    }
+}
